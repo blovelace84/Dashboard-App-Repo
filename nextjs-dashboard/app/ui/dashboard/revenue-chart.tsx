2d93e69e966f4ba5b40de60e28a27f79ca3ff4e8
Brittani Lovelace <blovelace84@gmail.com>
2025-11-07 10:58:32 -0500

    streaming

--- nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
+++ nextjs-dashboard/app/ui/dashboard/revenue-chart.tsx
@@ -10,14 +10,10 @@ import { fetchRevenue } from "@/app/lib/data";
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
-  const chartHeight = 350;
-  // NOTE: Uncomment this code in Chapter 7
+export default async function RevenueChart() {
+  const revenue = await fetchRevenue();
 
+  const chartHeight = 350;
   const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
   if (!revenue || revenue.length === 0) {
