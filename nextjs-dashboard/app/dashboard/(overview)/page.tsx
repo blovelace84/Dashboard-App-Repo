8f8bcb1b72789d3fa56b74067d5429c6f422ec06
Brittani Lovelace <blovelace84@gmail.com>
2025-11-07 08:43:06 -0500

    redoing fetching data

--- nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -3,10 +3,12 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import {
-  fetchRevenue,
   fetchLatestInvoices,
   fetchCardData,
+  fetchRevenue,
 } from "@/app/lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function Page() {
   const revenue = await fetchRevenue();
